dec21ed6434237eef74c601abddb43f2aae95b71
VinAU <L00179000@atu.ie>
2023-12-19 17:11:31 +0000

    created my own function named meow and called it

--- forloop.py
+++ forloop.py
@@ -1,8 +1,9 @@
-# taking the desired input from the user
-while True:
-    n = int(input("what is n ? "))
-    if n > 0:
-        break
-# printing the input desired number of times    
-for  i in range(n):
-    print("meow")
+#creating my own function
+def main():
+    meow(3)
+    
+def meow(n):
+    for _ in range(n):
+        print("meow")
+        
+main()
