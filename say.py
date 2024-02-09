01ba4777aa2d91936db8dd43315ab79046577ac7
Vinayak24git <vinayaksharmaofficial24@gmail.com>
2024-02-09 17:03:56 +0000

    learned about unit testing

--- say.py
+++ say.py
@@ -2,4 +2,4 @@ import cowsay
 import sys
 
 if len(sys.argv) == 2: # name of program + user input, then it will print. 
-    cowsay.trex("hello, " ,sys.argv[1])
+    cowsay.trex("hello, " +sys.argv[1])
